8bf5fd021433f9568999facdbb4fff2f12c7116c
rober97 <roberastorgab@gmail.com>
2023-01-22 21:50:30 -0300

    asd

--- src/layouts/ListUserLayout.vue
+++ src/layouts/ListUserLayout.vue
@@ -34,81 +34,127 @@
             <q-card-section>
               <div class="text-h6">Registrar nuevo usuario</div>
             </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="q-gutter-y-md column">
-                <div class="row">
-                  <div class="col-6">
-                    <q-input
-                      standout="bg-light-blue-4 text-white"
-                      v-model="text"
-                      label="Nombre"
-                      :dense="dense"
-                    />
+            <q-form method="post" @submit="newUser">
+              <q-card-section class="q-pt-none">
+                <div class="q-gutter-y-md column">
+                  <div class="row">
+                    <div class="col-6">
+                      <q-input
+                        standout="bg-light-blue-4 text-white"
+                        v-model="userObject.name"
+                        required
+                        label="Nombre"
+                      />
+                    </div>
+                    <div class="col-6 q-pl-sm">
+                      <q-input
+                        standout="bg-light-blue-4 text-white"
+                        v-model="userObject.lastname"
+                        required
+                        label="Apellido"
+                      />
+                    </div>
                   </div>
-                  <div class="col-6 q-pl-sm">
-                    <q-input
-                      standout="bg-light-blue-4 text-white"
-                      v-model="text"
-                      label="Apellido"
-                      :dense="dense"
-                    />
-                  </div>
-                </div>
 
-                <div class="row">
-                  <div class="col-12 col-md-12">
-                    <q-input
-                      standout="bg-light-blue-4 text-white"
-                      v-model="text"
-                      label="Rut"
-                      :dense="dense"
-                    />
+                  <div class="row">
+                    <div class="col-12 col-md-12">
+                      <q-input
+                        standout="bg-light-blue-4 text-white"
+                        v-model="userObject.rut"
+                        required
+                        label="Rut"
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div class="row">
-                  <div class="col-12 col-md-12">
-                    <q-input
-                      standout="bg-light-blue-4 text-white"
-                      v-model="text"
-                      label="Contraseña"
-                      :dense="dense"
-                    />
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-12 col-md-12">
-                    <q-input
-                      standout="bg-light-blue-4 text-white"
-                      v-model="text"
-                      label="Confirmar contraseña"
-                      :dense="dense"
-                    />
+                  <div class="row">
+                    <div class="col-12 col-md-12">
+                      <q-select
+                        filled
+                        v-model="userObject.type"
+                        required
+                        :options="options"
+                        label="Tipo de usuario"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </q-card-section>
+              </q-card-section>
 
-            <q-card-actions align="right" class="bg-white text-teal">
-              <q-btn flat label="Registrar" @click="newUser()" v-close-popup />
-            </q-card-actions>
+              <q-card-actions align="right" class="bg-white text-teal">
+                <!-- v-close-popup -->
+                <q-btn flat label="Registrar" type="submit" />
+              </q-card-actions>
+            </q-form>
           </q-card>
         </q-dialog>
 
         <q-table
+          ref="tableRef"
           title="Usuarios registrados"
-          :rows="records"
+          :rows="rows"
           :columns="columns"
-          row-key="name"
           selection="multiple"
-          v-model:selected="selected"
-          :hide-bottom="hideBottom"
-          :hide-selected-banner="hideSelectedBanner"
-          :hide-no-data="hideNoData"
-          :hide-pagination="hidePagination"
-        />
+          v-model:pagination="pagination"
+        >
+          <template v-slot:top-right>
+            <q-input
+              borderless
+              dense
+              debounce="300"
+              v-model="filter"
+              placeholder="Buscador"
+            >
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </template>
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th auto-width>
+                <q-checkbox dense v-model="selectAll" @click="selectAllRows" />
+              </q-th>
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                {{ col.label }}
+              </q-th>
+            </q-tr>
+          </template>
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td auto-width>
+                <q-checkbox dense v-model="props.row.selected" />
+              </q-td>
+              <q-td key="name" :props="props">
+                {{ props.row.name }}
+              </q-td>
+              <q-td key="lastname" :props="props">
+                {{ props.row.lastname }}
+              </q-td>
+
+              <q-td key="rut" :props="props">
+                {{ props.row.rut }}
+              </q-td>
+
+              <q-td key="type" :props="props">
+                <q-badge color="green">
+                  {{ props.row.type }}
+                </q-badge>
+              </q-td>
+
+              <q-td key="type" :props="props">
+                <q-btn
+                  size="sm"
+                  color="accent"
+                  round
+                  dense
+                  @click="deleteUser"
+                  :icon="'remove'"
+                />
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
       </div>
     </q-page-container>
   </q-layout>
@@ -118,90 +164,38 @@
 import { defineComponent, ref, computed } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
 import DrawerComponent from "components/DrawerComponent.vue";
+import axios from "axios";
 
 const columns = [
   {
     name: "name",
-    required: true,
-    label: "Dessert (100g serving)",
+    label: "Nombre",
     align: "left",
     field: (row) => row.name,
-    format: (val) => `${val}`,
     sortable: true,
   },
   {
-    name: "calories",
+    name: "lastname",
     align: "center",
-    label: "Calories",
-    field: "calories",
+    label: "Apellido",
+    field: (row) => row.lastname,
     sortable: true,
   },
-  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-  { name: "carbs", label: "Carbs (g)", field: "carbs" },
+  { name: "rut", label: "Rut", field: (row) => row.rut, sortable: true },
+  { name: "type", label: "Tipo", field: (row) => row.type },
+  { name: "actions", label: "Acciones" },
 ];
 
-const rows = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-  },
-  {
-    name: "Lollipop",
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-  },
-  {
-    name: "Honeycomb",
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-  },
-  {
-    name: "Donut",
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-  },
-  {
-    name: "KitKat",
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-  },
-];
+const rows = ref([]);
+
+const userObject = ref({
+  name: "",
+  lastname: "",
+  rut: "",
+  password: "",
+  confirm_password: "",
+  type: "",
+});
 
 export default defineComponent({
   name: "MainLayout",
@@ -215,13 +209,77 @@ export default defineComponent({
       this.$router.push({ path: "/login" });
     },
 
-    newUser() {
-      alert("ASD");
+    async newUser() {
+      let password = userObject.value.rut.substring(
+        4,
+        userObject.value.rut.length
+      );
+      let config = {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        url: "http://localhost:3000/new-user",
+        //timeout: 5000,
+        data: {
+          name: userObject.value.name,
+          apellido: userObject.value.lastname,
+          rut: userObject.value.rut,
+          type: userObject.value.type,
+          password,
+        },
+      };
+
+      await axios(config).then((respuestas) => {
+        debugger;
+      });
     },
+
+    async getUser() {
+      let config = {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+        url: "http://localhost:3000/list-user",
+      };
+
+      await axios(config).then((respuestas) => {
+        respuestas.data.forEach((element) => {
+          let res = {
+            _id: element._id,
+            name: element.nombre,
+            lastname: element.apellido,
+            rut: element.rut,
+            type: element.tipo,
+            selected: false,
+          };
+          rows.value.push(res);
+        });
+      });
+    },
+
+    async deleteUser() {},
+
+    selectAllRows() {
+      rows.value.forEach((v) => (v.selected = !v.selected));
+    },
+  },
+
+  async mounted() {
+    this.getUser();
   },
 
   setup() {
     const hasData = ref(true);
+    const pagination = ref({
+      sortBy: "desc",
+      descending: false,
+      page: 1,
+      rowsPerPage: 100,
+    });
 
     return {
       hasData,
@@ -229,10 +287,12 @@ export default defineComponent({
       hideSelectedBanner: ref(false),
       hideNoData: ref(false),
       hidePagination: ref(false),
+      selectAll: ref(false),
 
-      selected: ref([rows[1]]),
+      selected: ref([rows.value[1]]),
 
       columns,
+      text: ref(""),
 
       records: computed(() => (hasData.value === true ? rows : [])),
 
@@ -240,6 +300,11 @@ export default defineComponent({
       medium: ref(false),
       fullWidth: ref(false),
       fullHeight: ref(false),
+      options: ["Administrador", "Usuario"],
+      model: ref(null),
+      userObject,
+      rows,
+      pagination,
     };
   },
 });
